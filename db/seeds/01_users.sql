b74876670b2f08ca296814872b321ddc3ee53149
Stanley Lai <stanley.lai@telus.com>
2022-12-17 14:19:07 -0800

    Insert data for menu_items

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,13 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO menu_items (name, price, ingredients, inventory)
+VALUES
+('CHICKEN PESTO PIZZA', 14, 'Artichokes, Onions, Zucchini, Spinach, Feta, Tomatoes, Chicken Breast, Cheese, Basil & Pesto Drizzle', true),
+('HOUSE SPECIAL', 14, 'Ham, Pepperoni, Mushrooms, Onions, Green Peppers, Olives, Cheese & Tomato Sauce', true),
+('FETA SPECIAL', 14, 'Italian salami, Mushrooms, Onions, Tomatoes, Green Peppers, Feta Cheese, Cheese & Tomato Sauce', true),
+('HOT & SPICY SPECIAL', 14, 'Hot Pepperoni, Hot Salami, Mushrooms, Onions, Green Pepper, Cheese & Tomato Sauce', true),
+('EVERYTHING SPECIAL', 14, 'Ham, Pepperoni, Salami, Mushrooms, Lean Beef, Olives, Onions, Green Peppers, Pineapple, Tomatoes, Cheese & Tomato Sauce', true),
+('VEGETARIAN SPECIAL', 14, 'Mushroom, Olives, Onions, Pineapple, Tomatoes, Green Peppers, Cheese & Tomato Sauce', true),
+('VEGGIE GARLIC', 14, 'Mushrooms, Red Onion, Tomato, Spinach, Green Pepper, Artichoke Heart, Fresh Garlic, White Garlic Sauce', true),
+('TACO VEGGIE SPECIAL', 14, 'Red Onions, Jalapeno Pepper, Sour Cream, Paneer, Lettuce, Fresh Tomatoes & Cheese', true),
+('CHICKEN PESTO PIZZA', 14, 'Artichokes, Onions, Zucchini, Spinach, Feta, Tomatoes, Chicken Breast, Cheese, Basil & Pesto Drizzle', true),
+('CHICKEN PESTO PIZZA', 14, 'Artichokes, Onions, Zucchini, Spinach, Feta, Tomatoes, Chicken Breast, Cheese, Basil & Pesto Drizzle', true),
+('CHICKEN PESTO PIZZA', 14, 'Artichokes, Onions, Zucchini, Spinach, Feta, Tomatoes, Chicken Breast, Cheese, Basil & Pesto Drizzle', true),
