38ae1c20ab759d7dc304604dc6569f8a921f7feb
Eszter Egyud <eszter.egyud@telus.com>
2022-12-18 13:55:43 -0700

    Update and add to list of queries

--- db/queries/03_list_todays_orders.sql
+++ db/queries/03_list_todays_orders.sql
@@ -1,6 +1,5 @@
-SELECT * 
+SELECT *, CURRENT_DATE
 FROM orders 
-WHERE DATE(order_started) = CURRENT_DATE;
+WHERE date(order_started) = CURRENT_DATE;
 -- JOIN ordered_items ON order_id = orders.id 
 
--- this select is not working, have to find a way to filter for today's date !!!
