ee8ef8600cb548435ff64ccf6f73e9122c0a98c3
Stanley Lai <stanley.lai@telus.com>
2022-12-20 20:42:00 -0800

    add subtotal logic

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -12,19 +12,29 @@ $(() => {
       const foodItem = response.foodItem;
       for(const index in foodItem) {
         const item = foodItem[index];
-        $(`<tr class="menu">`).append(`<td>${item.name}</td><td>${item.price}</td><td><input type="button" value='-' id="qtyMin-${index}" onclick="qtyMin(this)"/><span id="qty-${index}">0</span><input type=
-        "button" value="+" id="qtyAdd-${index}" onclick="qtyAdd(this)"/><input type="button" value="Add to Cart"/>`).appendTo($menuList);
+        $(`<tr class="menu">`).append(`<td>${item.name}</td><td id=price-${index}>${item.price}</td><td><input type="button" value='-' id="qtyMin-${index}" onclick="qtyMin(this)"/><span id="qty-${index}">0</span><input type=
+        "button" value="+" id="qtyAdd-${index}" onclick="qtyAdd(this)"/>`).appendTo($menuList);
       }
 
     });
 });
 
 
+const priceCalculate = function (price) {
+  // let price = parseInt($(`#price-${index}`).text());
+  let subtotal = parseInt($(`#subtotal`).text());
+  subtotal += price;
+  $(`#subtotal`).text(subtotal);
+}
+
 const qtyAdd = function (element) {
   let index = $(element).attr('id').slice(7);
   let count = parseInt($(`#qty-${index}`).first().text());
   count++ ;
   $(`#qty-${index}`).first().text(count);
+  let price = parseInt($(`#price-${index}`).text());
+  priceCalculate(price);
+
 
 }
 
@@ -36,6 +46,8 @@ const qtyMin = function (element) {
   }
   count-- ;
   $(`#qty-${index}`).first().text(count);
+  let price = parseInt($(`#price-${index}`).text());
+  priceCalculate(-price);
 }
 
 
