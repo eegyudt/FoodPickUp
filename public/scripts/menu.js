a3afd4c6d130090563c090cee17ad786a8b8e852
Stanley Lai <stanley.lai@telus.com>
2022-12-20 16:06:38 -0800

    add a bit of plus/ minus sign button logic

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,6 +1,6 @@
 // Client facing scripts here
 $(() => {
-  $('#fetch-menu').on('click', () => {
+
     $.ajax({
       method: 'GET',
       url: '/api/menu'
@@ -9,12 +9,23 @@ $(() => {
       const $menuList = $('#menu');
       $menuList.empty();
 
-      for(const item of response.foodItem) {
-        $(`<tr class="menu">`).append(`<td>${item.name}</td><td>${item.price}</td><td><input type="button" value='-'/><span>0</span><input type=
-        "button" value="+"/><input type="button" value="Add to Cart"/>`).appendTo($menuList);
+      const foodItem = response.foodItem;
+      for(const index in foodItem) {
+        const item = foodItem[index];
+        $(`<tr class="menu">`).append(`<td>${item.name}</td><td>${item.price}</td><td><input type="button" value='-' id="qtyMinus-${index}" onclick="qtyMinus(this)"/><span>0</span><input type=
+        "button" value="+" id="qtyAdd-${index}" onclick="qtyAdd(this)"/><input type="button" value="Add to Cart"/>`).appendTo($menuList);
       }
 
     });
-  });
 });
 
+
+const qtyAdd = function (element) {
+console.log(element)
+
+}
+
+const qtyMinus = function (element) {
+  console.log(element)
+
+  }
