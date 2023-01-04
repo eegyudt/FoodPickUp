78fbe9d3f4e582cbfab35c65123600c89965f556
Eszter Egyud <eszter.egyud@telus.com>
2023-01-04 13:55:01 -0700

    Update send_sms.js to export sendText function

--- send_sms.js
+++ send_sms.js
@@ -3,14 +3,19 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 const myNumber = process.env.MY_PHONE_NUMBER;
-
+let textMessage = 'Luke! I am your father!';
 const client = require('twilio')(accountSid, authToken);
 
+const sendText = (message, phoneNumber) => {
+  client.messages
+    .create({
+      body: message,
+      from: twilioNumber,
+      to: phoneNumber
+    })
+    .then(message => console.log(message.sid));
+};
 
-client.messages
-  .create({
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: twilioNumber,
-    to: myNumber
-  })
-  .then(message => console.log(message.sid));
+sendText(textMessage, myNumber);
+
+module.exports = sendText;
