14da19f1076b9fa0b066cf39f0a687fa2f1984c3
Eszter Egyud <eszter.egyud@telus.com>
2023-01-05 12:51:10 -0700

    Implement jquery script for admin page sendSMS button

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,21 +1,27 @@
 
-const sendText = require('../send_sms');
-
 $(() => {
 
-  $.ajax({
-    method: 'POST',
-    url: '/admin'
-  })
-    .done((response) => {
-
-      const $number = $('#phoneSMS');
 
 
-      $('#sendSMS').click(function () {
-        console.log($number)
-      })
+  $('#sendSMS').on('click', function() {
+    const detail = $(this).closest('.orderDetail');
+    const orderid = detail.find('.orderid').text();
+    const phoneNumber = detail.find('.phoneSMS').text();
+
+    const timeframe = detail.find('.timeframe').val();
+    const data = { orderid, phoneNumber, timeframe };
+    console.log("orderid, phoneNumber, timeframe >>>>>", data);
+
+    $.post('/admin', data)
+      .then(res => {
+        console.log("RES>>>>>>>>>", res);
+      });
 
 
-    });
+
+
+
+  });
+
+
 });
